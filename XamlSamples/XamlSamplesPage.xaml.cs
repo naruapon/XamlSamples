4ddbe9d6930a777582e3b9a9b1f64aed276cb012
naruapon <naruapon@gmail.com>
2017-12-16 00:00:05 +0700

    Initial commit

---
+++ XamlSamples/XamlSamplesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace XamlSamples
+{
+    public partial class XamlSamplesPage : ContentPage
+    {
+        public XamlSamplesPage()
+        {
+            InitializeComponent();
+            Button button = new Button
+            {
+                Text = "Navigate!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new HelloXamlPage());
+            };
+
+            Content = button;
+        }
+    }
+}
